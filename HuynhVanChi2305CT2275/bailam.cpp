a772652131cc397fbe5619f3edf1b8b2a8e1ef7e
HuynhVanChi <huynhvanchi978@gmail.com>
2024-04-20 14:26:51 +0700

    fadsf

--- HuynhVanChi2305CT2275/bailam.cpp
+++ HuynhVanChi2305CT2275/bailam.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 int main() {
-    // Phần 1: Khai báo và khởi tạo mảng chứa MSSV
-    string danhsachMSSV[5] = { "", "2301CT1234", "2302CT3456", "2305CT3999", "2304CT0002" };
+    // Phần 1: Khai bao va khoi tao mang chua MSSV
+    string danhsachMSSV[5] = { "0", "2303CT2275", "2302CT2286", "2301CT2293", "2304CT2295" };
 
-    // Phần 2: Yêu cầu người dùng nhập MSSV của mình
+    // Phần 2: Yeu cau ngoui dung nhap MSSV của minh
     cout << "Nhập mã số sinh viên của bạn: ";
     getline(cin, danhsachMSSV[0]);
 
-    // Phần 3: In mảng ra màn hình
+    // Phần 3: In mang ra man hinh
     cout << "Danh sách MSSV:" << endl;
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; i++) {
         cout << danhsachMSSV[i] << endl;
     }
 
-    // Phần 4: Sắp xếp mảng theo thứ tự từ thấp đến cao
+    // Phan 4: Sap xep mang theo thu tu tu thap đen cao
     string temp;
-    for (int i = 0; i < 5 - 1; ++i) {
+    for (int i = 0; i < 5 - 1; i++) {
         for (int j = 0; j < 5 - i - 1; ++j) {
             if (danhsachMSSV[j] > danhsachMSSV[j + 1]) {
                 temp = danhsachMSSV[j];
@@ -28,22 +28,22 @@ int main() {
         }
     }
 
-    // In mảng đã sắp xếp ra màn hình
-    cout << "\nDanh sách MSSV đã sắp xếp:" << endl;
-    for (int i = 0; i < 5; ++i) {
+    // In mang da sap xep ra man hinh
+    cout << "\nDanh sach MSSV đã sap xep:" << endl;
+    for (int i = 0; i < 5; i++) {
         cout << danhsachMSSV[i] << endl;
     }
 
-    // Phần 5: Tìm địa chỉ và in phần tử chứa MSSV lớn nhất
+    // Phan 5: Tim dia chi và in phan tu chua MSSV lon nhat
     string *ptrArrayMSSV = danhsachMSSV;
     string *maxMSSV = ptrArrayMSSV;
-    for (int i = 1; i < 5; ++i) {
+    for (int i = 1; i < 5; i++) {
         if (*ptrArrayMSSV < danhsachMSSV[i]) {
             maxMSSV = ptrArrayMSSV + i;
         }
     }
-    cout << "\nĐịa chỉ của phần tử chứa MSSV lớn nhất: " << maxMSSV << endl;
-    cout << "MSSV lớn nhất: " << *maxMSSV << endl;
+    cout << "Đia chi của phan tu chua MSSV lon nhat: " << maxMSSV << endl;
+    cout << "MSSV lon nhat: " << *maxMSSV << endl;
 
     return 0;
 }
